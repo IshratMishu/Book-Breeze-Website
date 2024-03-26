e0dd397b27911e9cc79611d79d3e3c6c7c81feb6
IshratMishu <ishratjahan0425@gmail.com>
2024-03-26 16:52:25 +0600

    sort by function done

--- src/components/ListedBooks/ListedBooks.jsx
+++ src/components/ListedBooks/ListedBooks.jsx
@@ -9,12 +9,27 @@ import { NavLink } from "react-router-dom";
 
 const ListedBooks = () => {
     const [readList, setReadList] = useState([]);
-    const [wishList, setWishList] = useState([]);
+    // const [wishList, setWishList] = useState([]);
+    
+    const [displayBooks, setDisplayBooks] = useState([]);
+
+    const handleFilter = filter =>{
+        let sortedBooks = [...readList]; 
+        if (filter === 'rating') {
+            sortedBooks.sort((a, b) => b.rating - a.rating);
+        } else if (filter === 'totalPages') {
+            sortedBooks.sort((a, b) => b.totalPages - a.totalPages);
+        } else if (filter === 'yearOfPublishing') {
+            sortedBooks.sort((a, b) => b.yearOfPublishing - a.yearOfPublishing);
+        }
+        setDisplayBooks(sortedBooks);
+    };
 
     useEffect(() => {
         const book = getBookFromLocalStorage()
         setReadList(book);
-        setWishList(book);
+        // setWishList(book);
+        setDisplayBooks(book);
     }, [])
 
     return (
@@ -27,9 +42,9 @@ const ListedBooks = () => {
                 <details className="dropdown">
                     <summary className="m-1 btn">Sort By <span><RiArrowDropDownLine /></span></summary>
                     <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
-                        <li><a>Rating</a></li>
-                        <li><a>Number of Pages</a></li>
-                        <li><a>Publisher Year</a></li>
+                        <li><a onClick={()=>handleFilter('rating')}>Rating</a></li>
+                        <li><a onClick={()=>handleFilter('totalPages')}>Number of Pages</a></li>
+                        <li><a onClick={()=>handleFilter('yearOfPublishing')}>Publisher Year</a></li>
                     </ul>
                 </details>
             </div>
@@ -40,7 +55,7 @@ const ListedBooks = () => {
                 <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Read Books" checked />
                 <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">
                     {
-                        readList.map((book, idx) => <div key={idx} className="card card-side bg-base-100 shadow-xl">
+                        displayBooks.map((book, idx) => <div key={idx} className="card card-side bg-base-100 shadow-xl">
                             <figure><img src={book.image} alt="Movie" /></figure>
                             <div className="card-body">
                                 <h2 className="card-title">{book.bookName}</h2>
@@ -87,7 +102,7 @@ const ListedBooks = () => {
                 <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Wishlist Books" />
                 <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">
                     {
-                        wishList.map((book, idx) => <div key={idx} className="card card-side bg-base-100 shadow-xl">
+                        displayBooks.map((book, idx) => <div key={idx} className="card card-side bg-base-100 shadow-xl">
                             <figure><img src={book.image} alt="Movie" /></figure>
                             <div className="card-body">
                                 <h2 className="card-title">{book.bookName}</h2>

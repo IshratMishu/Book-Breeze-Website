566b647f5e6732a3fc14172919b996924ea83fa7
IshratMishu <ishratjahan0425@gmail.com>
2024-03-27 16:23:44 +0600

    Tailwind css applied in navbar and banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,12 +3,12 @@ import { NavLink } from "react-router-dom";
 
 const Banner = () => {
     return (
-        <div className="hero min-h-screen bg-base-200">
-            <div className="hero-content flex-col lg:flex-row-reverse">
-                <img src="https://i.ibb.co/rbsH0sR/the-bike.png" className="max-w-sm rounded-lg shadow-2xl" />
-                <div>
-                    <h1 className="text-5xl font-bold">Refreshing Your Bookshelf with New Reads</h1>
-                    <NavLink to='/listed'><button className="btn btn-primary">View The List</button></NavLink>
+        <div className="hero h-[520px] md:h-[600px] bg-base-200 font-workSans p-2 md:p-10 rounded-xl">
+            <div className="hero-content text-center md:text-start flex-col md:flex-row-reverse">
+                <img src="https://i.ibb.co/rbsH0sR/the-bike.png" className="md:w-auto w-60"/>
+                <div className="md:p-15 p-4">
+                    <h1 className="md:text-5xl text-2xl font-bold">Refreshing Your Bookshelf with New Reads</h1>
+                    <NavLink to='/listed'><button className="btn bg-[#23BE0A] text-white font-medium md:font-bold text-lg md:mt-4 mt-2">View The List</button></NavLink>
                 </div>
             </div>
         </div>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,35 +3,35 @@ import { NavLink } from "react-router-dom";
 const Header = () => {
 
     const links = <>
-        <li><NavLink to='/'>Home</NavLink></li>
-        <li><NavLink to='/listed'>Listed Books</NavLink></li>
-        <li><NavLink to='/read'>Pages to Read</NavLink></li>
-        <li><NavLink to='/bestseller'>Offers</NavLink></li>
-        <li><NavLink to='/offers'>Best Seller</NavLink></li>
-        <li><NavLink to='/box'>Mystery Box</NavLink></li>
+        <li><NavLink to='/' className={({ isActive}) => isActive ? 'text-[#23BE0A] border-2 border-[#23BE0A]' : 'text-gray-500'}>Home</NavLink></li>
+        <li><NavLink to='/listed' className={({ isActive }) => isActive ? 'text-[#23BE0A] border-2 border-[#23BE0A]' : 'text-gray-500'}>Listed Books</NavLink></li>
+        <li><NavLink to='/read' className={({ isActive }) => isActive ? 'text-[#23BE0A] border-2 border-[#23BE0A]' : 'text-gray-500'}>Pages to Read</NavLink></li>
+        <li><NavLink to='/bestseller' className={({ isActive }) => isActive ? 'text-[#23BE0A] border-2 border-[#23BE0A]' : 'text-gray-500'}>Offers</NavLink></li>
+        <li><NavLink to='/offers' className={({ isActive }) => isActive ? 'text-[#23BE0A] border-2 border-[#23BE0A]' : 'text-gray-500'}>Best Seller</NavLink></li>
+        <li><NavLink to='/box' className={({ isActive }) => isActive ? 'text-[#23BE0A] border-2 border-[#23BE0A]' : 'text-gray-500'}>Mystery Box</NavLink></li>
     </>
 
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-base-100 font-workSans">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </div>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 font-semibold">
                         {links}
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">BookBreeze</a>
+                <a className="btn btn-ghost text-3xl font-bold gap-0">Book<span className="text-[#23BE0A]">Breeze</span></a>
             </div>
             <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
+                <ul className="menu menu-horizontal px-1 font-semibold">
                     {links}
                 </ul>
             </div>
-            <div className="navbar-end">
-                <a className="btn">Sign In</a>
-                <a className="btn">Sign Up</a>
+            <div className="navbar-end gap-2 hidden md:flex">
+                <a className="btn bg-[#23BE0A] text-white">Sign In</a>
+                <a className="btn bg-info text-white">Sign Up</a>
             </div>
         </div>
     );
